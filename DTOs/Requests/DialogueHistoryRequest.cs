abceedcc52c41619d30a906d28d4edf39f6d5868
Fyn <1835991478@qq.com>
2026-03-17 22:14:08 +0800

    Update DialogueHistoryRequest.cs

---
+++ DTOs/Requests/DialogueHistoryRequest.cs
@@ -0,0 +1,7 @@
+namespace NPCDialogueServer.DTOs.Requests;
+
+public class DialogueHistoryRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string NpcName { get; set; } = string.Empty;
+}
