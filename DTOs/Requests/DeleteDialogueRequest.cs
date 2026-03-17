9115cded1225cb1c4f601303de681d51489fe650
Fyn <1835991478@qq.com>
2026-03-17 22:14:58 +0800

    Update DeleteDialogueRequest.cs

---
+++ DTOs/Requests/DeleteDialogueRequest.cs
@@ -0,0 +1,7 @@
+namespace NPCDialogueServer.DTOs.Requests;
+
+public class DeleteDialogueRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string NpcName { get; set; } = string.Empty;
+}
