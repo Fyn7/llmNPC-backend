2bb645446b60d271fb39643e7e277512f662eecb
Fyn <1835991478@qq.com>
2026-03-17 22:15:21 +0800

    Update DialogueResponse.cs

---
+++ DTOs/Responses/DialogueResponse.cs
@@ -0,0 +1,7 @@
+namespace NPCDialogueServer.DTOs.Responses;
+
+public class DialogueResponse
+{
+    public string Role { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+}
